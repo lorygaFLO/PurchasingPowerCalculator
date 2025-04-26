18a9507efa0bfb9e77d83883e189b3c2e0595ddc
lorygaFLO <38256023+lorygaFLO@users.noreply.github.com>
2025-04-26 15:48:33 +0200

    feat: report and scraper
    ply attention, the scraper posecute if habit cost file is not compiled

--- utils/scraper.py
+++ utils/scraper.py
@@ -20,7 +20,7 @@ def get_cost_of_living_data(year=2023):
         pandas.DataFrame: DataFrame containing cost of living data
     """
     # URL di Numbeo con tutti gli item ID per le diverse categorie di spesa
-    url = f'https://www.numbeo.com/cost-of-living/historical-prices-by-country?displayCurrency=EUR&year={year}&itemId=101&itemId=100&itemId=228&itemId=224&itemId=60&itemId=66&itemId=64&itemId=62&itemId=110&itemId=118&itemId=121&itemId=14&itemId=19&itemId=17&itemId=15&itemId=11&itemId=16&itemId=113&itemId=9&itemId=12&itemId=8&itemId=119&itemId=111&itemId=112&itemId=115&itemId=116&itemId=13&itemId=27&itemId=26&itemId=29&itemId=28&itemId=114&itemId=6&itemId=4&itemId=5&itemId=3&itemId=2&itemId=1&itemId=7&itemId=105&itemId=106&itemId=44&itemId=40&itemId=42&itemId=24&itemId=20&itemId=18&itemId=109&itemId=108&itemId=107&itemId=206&itemId=25&itemId=30&itemId=33&itemId=34'
+    url = f'https://www.numbeo.com/cost-of-living/historical-prices-by-country?displayCurrency=EUR&year={year}&itemId=228&itemId=224&itemId=60&itemId=66&itemId=64&itemId=62&itemId=110&itemId=118&itemId=121&itemId=14&itemId=19&itemId=17&itemId=15&itemId=11&itemId=16&itemId=113&itemId=9&itemId=12&itemId=8&itemId=119&itemId=111&itemId=112&itemId=115&itemId=116&itemId=13&itemId=27&itemId=26&itemId=29&itemId=28&itemId=114&itemId=6&itemId=4&itemId=5&itemId=3&itemId=2&itemId=1&itemId=7&itemId=105&itemId=106&itemId=44&itemId=40&itemId=42&itemId=24&itemId=20&itemId=18&itemId=109&itemId=108&itemId=107&itemId=30&itemId=33&itemId=34'
             
     try:
         # Download the page
